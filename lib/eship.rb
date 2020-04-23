8cad208eafd2a510727e4878e20f23d53429bd81
Gabriel Dzul <gabriel@yellowme.mx>
2020-04-23 12:56:45 -0500

    Added basic structure

---
+++ lib/eship.rb
@@ -0,0 +1,29 @@
+require 'eship/version.rb'
+
+module Eship
+
+  def self.config
+    yield self
+  end
+
+  def self.version
+    @version
+  end
+
+  def self.base_uri
+    @base_uri
+  end
+
+  def self.base_uri=(base_uri)
+    @base_uri = base_uri
+  end
+
+
+  def self.eship_key
+    @eship_key
+  end
+
+  def self.eship_key=(eship_key)
+    @eship_key = cacao_key
+  end
+end
