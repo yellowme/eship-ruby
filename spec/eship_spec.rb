f7be755646a403556868a1d05141f591b2449a26
Gabriel Dzul <gabriel@yellowme.mx>
2020-04-23 14:50:00 -0500

    Added configuration test

--- spec/eship_spec.rb
+++ spec/eship_spec.rb
@@ -1,4 +1,20 @@
 RSpec.describe Eship do
+  it 'configures eship gem' do
+    # Given
+    base_uri = 'http://google.com'
+    eship_key = 'random_key1'
+
+    # When
+    Eship.config do |c|
+      c.base_uri = base_uri
+      c.eship_key = eship_key
+    end
+
+    # Then
+    expect(Eship.base_uri).to eq base_uri
+    expect(Eship.eship_key).to eq eship_key
+  end
+
   it "has a version number" do
     expect(Eship::VERSION).not_to be nil
   end
