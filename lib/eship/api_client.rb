6ef35ad7a63f44905cfaee68f12e73f7c3ca8be7
Gabriel Dzul <gabriel@yellowme.mx>
2020-04-24 15:28:40 -0500

    Added ApiClient module

--- lib/eship/api_client.rb
+++ lib/eship/api_client.rb
@@ -1,10 +1,33 @@
 # frozen_string_literal: true
+require 'httparty'
 
 module Eship
   module ApiClient
-    def self.post(path:, body:, headers: {})
-      response = Eship.connection.post path, body, headers
-      response.body
+    def self.default_headers
+      {
+        "content-type" => 'application/json',
+        ImmutableKey.new("api-key") => Eship.eship_key 
+      }
+    end
+
+    def self.post(path:, body: {}, headers: {})
+      path = "#{Eship.base_uri}#{path}"
+      headers = headers.merge(ApiClient.default_headers)
+      response = HTTParty.post(path, body: body.to_json, headers: headers)
+      JSON.parse(response.body)
+    end
+
+    #Monkey patch to avoid headers capitalization https://jatindhankhar.in/blog/custom-http-header-and-ruby-standard-library/
+    class ImmutableKey < String 
+      def capitalize 
+        self 
+      end
+
+      def to_s
+        self 
+       end 
+
+       alias_method :to_str, :to_s
     end
   end
 end
